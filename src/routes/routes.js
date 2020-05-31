e4c1d22705e721062b7fcaacb1ff62d9fb0ffa89
FabioKido <fabiohenryquemesquita@gmail.com>
2020-05-31 13:57:49 -0300

    Feat: Password Recovery Page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,17 +3,27 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import createAnimatedSwitchNavigator from 'react-navigation-animated-switch';
 import { Transition } from 'react-native-reanimated';
 
+// Public Routes
+
 import Login from '../pages/Login';
+import PasswordRecover from '../pages/PasswordRecover';
 import Logout from '../pages/Logout';
-import Dashboard from '../pages/Dashboard';
 import Step1 from '../pages/Register/step1';
 import Step2 from '../pages/Register/step2';
+
+// Private Routes
+
+import Dashboard from '../pages/Dashboard';
+
+// Não é esse navigation é o Stack com header(com opção voltar).
+// Adicionar um Tab navigation também...
 
 const Routes = createAppContainer(
   createAnimatedSwitchNavigator(
     {
       Logout,
       Login,
+      PasswordRecover,
       Dashboard,
       Step1,
       Step2
