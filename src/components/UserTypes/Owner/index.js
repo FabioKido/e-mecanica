9411675107885ad55457b585d632e791432e0692
FabioKido <fabiohenryquemesquita@gmail.com>
2020-06-15 17:26:59 -0300

    TODO: Resolve date type

--- src/components/UserTypes/Owner/index.js
+++ src/components/UserTypes/Owner/index.js
@@ -26,12 +26,11 @@ import {
   SubmitButtonText,
 } from './styles';
 
-// Terminar de resolver a validação da data de aniversário:
-//    - Resolver no back o novo tipo de data para as datas importantes apenas para o front/mobile;
+// FIXME Terminar de resolver a validação da data de aniversário:
 //    - Resolver conversão da data vinda do banco para o front/mobile;
-//    - Resolver a conversão para salvar no banco depois;
+//    - Resolver a conversão da string para salvar no banco depois;
 //
-// Resolver a validação do CPF e CNPJ;
+// FIXME Resolver a validação do CPF e CNPJ;
 
 export default function Owner() {
 
@@ -49,19 +48,19 @@ export default function Owner() {
   const [rg, setRG] = useState('');
   const [birthday, setBirthday] = useState('');
   const [orgao_expeditor, setOrgaoExpeditor] = useState('');
-  
+
   const [date, setDate] = useState(new Date());
   const [loading, setLoading] = useState(false);
 
   const onDateChange = date => {
     setDate(date);
-    
+
     setBirthday(date);
   };
 
   useEffect(() => {
     async function getInfos() {
-      try{
+      try {
         setLoading(true);
 
         const response = await api.get(`/user/owner/${profile.id}`);
@@ -82,13 +81,13 @@ export default function Owner() {
         setRG(rg);
         setBirthday(birthday);
         setOrgaoExpeditor(orgao_expeditor);
-      }catch(err) {
+      } catch (err) {
         console.log(err);
-      }finally{
+      } finally {
         setLoading(false);
       }
     }
-    
+
     getInfos();
   }, []);
 
@@ -135,116 +134,116 @@ export default function Owner() {
         <FormContainer>
           <Title>PERFIL</Title>
           <Description>
-            Atualize suas informaçoes pessoais editando os campos abaixo, logo depois, clique em Salvar. 
+            Atualize suas informaçoes pessoais editando os campos abaixo, logo depois, clique em Salvar.
           </Description>
 
-            <InputTitle>NOME</InputTitle>
-            <InputContainer>
-              <Input
-                  placeholder="Digite seu nome"
-                  autoCapitalize="words"
-                  autoCorrect={false}
-                  onChangeText={setName}
-                  value={name}
-                  returnKeyType="next"
-                  onSubmitEditing={() => sexInputRef.current.focus()}
-              />
-              <MaterialIcons name="person-pin" size={20} color="#999" />
-            </InputContainer>
+          <InputTitle>NOME</InputTitle>
+          <InputContainer>
+            <Input
+              placeholder="Digite seu nome"
+              autoCapitalize="words"
+              autoCorrect={false}
+              onChangeText={setName}
+              value={name}
+              returnKeyType="next"
+              onSubmitEditing={() => sexInputRef.current.focus()}
+            />
+            <MaterialIcons name="person-pin" size={20} color="#999" />
+          </InputContainer>
 
-            <InputTitle>SEXO</InputTitle>
-            <InputContainer>
-              <Input
-                  placeholder="Seu sexo é"
-                  autoCapitalize="none"
-                  autoCorrect={false}
-                  maxLength={1}
-                  ref={sexInputRef}
-                  onChangeText={setSex}
-                  value={sex}
-                  returnKeyType="next"
-                  onSubmitEditing={() => cpfInputRef.current.focus()}
-              />
-              <MaterialIcons name="lock" size={20} color="#999" />
-            </InputContainer>
+          <InputTitle>SEXO</InputTitle>
+          <InputContainer>
+            <Input
+              placeholder="Seu sexo é"
+              autoCapitalize="none"
+              autoCorrect={false}
+              maxLength={1}
+              ref={sexInputRef}
+              onChangeText={setSex}
+              value={sex}
+              returnKeyType="next"
+              onSubmitEditing={() => cpfInputRef.current.focus()}
+            />
+            <MaterialIcons name="lock" size={20} color="#999" />
+          </InputContainer>
 
-            <InputTitle>ANIVERSÁRIO</InputTitle>
-            <InputContainer>
-              <Input
-                  placeholder="Clique no calendário para editar"
-                  editable={false}
-                  onChangeText={setBirthday}
-                  value={birthday}
-              />
-              <DatePicker
-                date={date}
-                is24Hour={true}
-                format="DD-MM-YYYY"
-                minDate="01-01-2001"
-                maxDate="31-12-2030"
-                hideText={true}
-                iconComponent={<FontAwesome5 name="calendar-alt" size={18} color="#999" />}
-                style={{
-                  width: 21
-                }}
-                onDateChange={onDateChange}
-              />
-            </InputContainer>
+          <InputTitle>ANIVERSÁRIO</InputTitle>
+          <InputContainer>
+            <Input
+              placeholder="Clique no calendário para editar"
+              editable={false}
+              onChangeText={setBirthday}
+              value={birthday}
+            />
+            <DatePicker
+              date={date}
+              is24Hour={true}
+              format="DD-MM-YYYY"
+              minDate="01-01-2001"
+              maxDate="31-12-2030"
+              hideText={true}
+              iconComponent={<FontAwesome5 name="calendar-alt" size={18} color="#999" />}
+              style={{
+                width: 21
+              }}
+              onDateChange={onDateChange}
+            />
+          </InputContainer>
 
-            <InputTitle>CPF</InputTitle>
-            <InputContainer>
-              <Input
-                  placeholder="Número do seu CPF"
-                  autoCapitalize="none"
-                  autoCorrect={false}
-                  maxLength={14}
-                  ref={cpfInputRef}
-                  onChangeText={setCPF}
-                  value={cpf}
-                  returnKeyType="next"
-                  onSubmitEditing={() => rgInputRef.current.focus()}
-              />
-              <MaterialIcons name="lock" size={20} color="#999" />
-            </InputContainer>
+          <InputTitle>CPF</InputTitle>
+          <InputContainer>
+            <Input
+              placeholder="Número do seu CPF"
+              autoCapitalize="none"
+              autoCorrect={false}
+              maxLength={14}
+              ref={cpfInputRef}
+              onChangeText={setCPF}
+              value={cpf}
+              returnKeyType="next"
+              onSubmitEditing={() => rgInputRef.current.focus()}
+            />
+            <MaterialIcons name="lock" size={20} color="#999" />
+          </InputContainer>
 
-            <InputTitle>RG</InputTitle>
-            <InputContainer>
-              <Input
-                  placeholder="Digite o seu RG"
-                  autoCapitalize="none"
-                  autoCorrect={false}
-                  maxLength={14}
-                  ref={rgInputRef}
-                  onChangeText={setRG}
-                  value={rg}
-                  returnKeyType="next"
-                  onSubmitEditing={() => orgaoExpeditorInputRef.current.focus()}
-              />
-              <MaterialIcons name="lock" size={20} color="#999" />
-            </InputContainer>
+          <InputTitle>RG</InputTitle>
+          <InputContainer>
+            <Input
+              placeholder="Digite o seu RG"
+              autoCapitalize="none"
+              autoCorrect={false}
+              maxLength={14}
+              ref={rgInputRef}
+              onChangeText={setRG}
+              value={rg}
+              returnKeyType="next"
+              onSubmitEditing={() => orgaoExpeditorInputRef.current.focus()}
+            />
+            <MaterialIcons name="lock" size={20} color="#999" />
+          </InputContainer>
 
-            <InputTitle>ORGÃO EXPEDITOR</InputTitle>
-            <InputContainer>
-              <Input
-                  placeholder="Orgão de expedição SSP/Outros"
-                  autoCapitalize="none"
-                  autoCorrect={false}
-                  maxLength={30}
-                  ref={orgaoExpeditorInputRef}
-                  onChangeText={setOrgaoExpeditor}
-                  value={orgao_expeditor}
-                  returnKeyType="send"
-                  onSubmitEditing={handleSaveOwner}
-              />
-              <MaterialIcons name="lock" size={20} color="#999" />
-            </InputContainer>
+          <InputTitle>ORGÃO EXPEDITOR</InputTitle>
+          <InputContainer>
+            <Input
+              placeholder="Orgão de expedição SSP/Outros"
+              autoCapitalize="none"
+              autoCorrect={false}
+              maxLength={30}
+              ref={orgaoExpeditorInputRef}
+              onChangeText={setOrgaoExpeditor}
+              value={orgao_expeditor}
+              returnKeyType="send"
+              onSubmitEditing={handleSaveOwner}
+            />
+            <MaterialIcons name="lock" size={20} color="#999" />
+          </InputContainer>
 
           <SubmitButton onPress={handleSaveOwner}>
             {loading ? (
               <ActivityIndicator size="small" color="#FFF" />
             ) : (
-              <SubmitButtonText>Salvar</SubmitButtonText>
-            )}
+                <SubmitButtonText>Salvar</SubmitButtonText>
+              )}
           </SubmitButton>
         </FormContainer>
       </Content>
