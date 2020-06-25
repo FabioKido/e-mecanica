5efdbab678dce17a6b8e272c2a934f299dba6c1c
FabioKido <fabiohenryquemesquita@gmail.com>
2020-06-25 15:44:48 -0300

    Updated: Page Customers Dashboard

--- src/pages/CustomerModule/Dashboard/index.js
+++ src/pages/CustomerModule/Dashboard/index.js
@@ -1,8 +1,64 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+
+import { LinearGradient } from 'expo-linear-gradient';
+
+import { MaterialIcons, FontAwesome5 } from '@expo/vector-icons';
+
+import {
+  Container,
+  Content,
+  FormContainer,
+  InputContainer,
+  Title,
+  Description,
+  InputTitle,
+  Input,
+  ChoiceButton,
+  ChoiceText,
+  SubmitButton,
+  SubmitButtonText,
+  CancelarButton,
+  CancelarButtonText,
+  Cards,
+  Card,
+  CardInfo,
+  CardTitle,
+  CardContainer,
+  CardName,
+  CardSubName,
+  CardStatus,
+  Empty
+} from './styles';
 
 export default function Dashboard() {
 
-  return <SafeAreaView />
+  return (
+    <LinearGradient
+      colors={['#2b475c', '#000']}
+      style={{ flex: 1 }}
+    >
+      <Container>
+        <Content keyboardShouldPersistTaps="handled">
+          <FormContainer>
+            <Title>Dashboard</Title>
+            <Description>
+              Visão geral sobre o modulo de clientes. Tenha informções importantes e as use como forma de melhorar seu negócio.
+            </Description>
 
+
+
+          </FormContainer>
+
+        </Content>
+      </Container>
+    </LinearGradient>
+  );
 }
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Dashboard',
+  tabBarIcon: ({ tintColor }) => (
+    <FontAwesome5 name="user-cog" size={18} color={tintColor} />
+  ),
+};
